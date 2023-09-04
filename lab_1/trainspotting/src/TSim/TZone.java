b845d52145be9fd9e25681180befbe90b39cf514
NacasNils <122795594+NacasNils@users.noreply.github.com>
2023-09-04 14:50:51 +0200

    Added TZone class that handles zone logic
    Each zone has a pair of SensorEvents in a array. The idea is that when a sensor activates the zone toggles through the classes own toggle method. all of this should be usefull to keep track of where our trains are to keep them from colliding

---
+++ lab_1/trainspotting/src/TSim/TZone.java
@@ -0,0 +1,26 @@
+package TSim;
+
+
+public class TZone {
+    public String   TZoneName;
+    public SensorEvent[] sensorPair;
+    public boolean active;
+    
+    public TZone( String name , SensorEvent x , SensorEvent y ){
+        this.TZoneName  = name;
+        this.sensorPair = new SensorEvent[2];
+        this.sensorPair[0] = x;
+        this.sensorPair[1] = y;
+        this.active = false;
+    } 
+
+    // toggleZone
+    //      If a sensor becomes active:
+    //      toggle "active" boolean.
+
+     void toggle (){
+        if (active == false) { active = true;  }
+        else                 { active = false; }
+    }
+}
+
