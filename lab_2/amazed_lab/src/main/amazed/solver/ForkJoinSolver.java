dd31aaa3468fb64e5c538d1253515f25b22c6719
Sebastian Pålsson <seb.plson@gmail.com>
2023-09-20 23:43:08 +0200

    format code

--- lab_2/amazed_lab/src/main/amazed/solver/ForkJoinSolver.java
+++ lab_2/amazed_lab/src/main/amazed/solver/ForkJoinSolver.java
@@ -19,30 +19,28 @@ import java.util.concurrent.ConcurrentSkipListSet;
  * <code>ForkJoinPool</code> object.
  */
 
-
 public class ForkJoinSolver
-    extends SequentialSolver{
-        
+        extends SequentialSolver {
+
     /**
      * Creates a solver that searches in <code>maze</code> from the
      * start node to a goal.
      *
-     * @param maze   the maze to be searched
+     * @param maze the maze to be searched
      */
 
-     // class to create new "threads" when forking 
-protected class FThread{
-            final int forkStart;
-            final ForkJoinSolver fthread;
+    // class to create new "threads" when forking
+    protected class FThread {
+        final int forkStart;
+        final ForkJoinSolver fthread;
 
-            FThread(int forkStart , ForkJoinSolver fthread){
-                this.forkStart = forkStart;
-                this.fthread = fthread;
-            }
+        FThread(int forkStart, ForkJoinSolver fthread) {
+            this.forkStart = forkStart;
+            this.fthread = fthread;
         }
+    }
 
-    public ForkJoinSolver(Maze maze)
-    {
+    public ForkJoinSolver(Maze maze) {
         super(maze);
     }
 
@@ -51,14 +49,13 @@ protected class FThread{
      * start node to a goal, forking after a given number of visited
      * nodes.
      *
-     * @param maze        the maze to be searched
-     * @param forkAFter   the number of steps (visited nodes) after
-     *                    which a parallel task is forked; if
-     *                    <code>forkAfter &lt;= 0</code> the solver never
-     *                    forks new tasks
+     * @param maze      the maze to be searched
+     * @param forkAFter the number of steps (visited nodes) after
+     *                  which a parallel task is forked; if
+     *                  <code>forkAfter &lt;= 0</code> the solver never
+     *                  forks new tasks
      */
-    public ForkJoinSolver(Maze maze, int forkAfter)
-    {
+    public ForkJoinSolver(Maze maze, int forkAfter) {
         this(maze);
         this.forkAfter = forkAfter;
     }
@@ -70,20 +67,18 @@ protected class FThread{
      * goals, or all goals are unreacheable), the method returns
      * <code>null</code>.
      *
-     * @return   the list of node identifiers from the start node to a
-     *           goal node in the maze; <code>null</code> if such a path cannot
-     *           be found.
+     * @return the list of node identifiers from the start node to a
+     *         goal node in the maze; <code>null</code> if such a path cannot
+     *         be found.
      */
     AtomicBoolean hasFoundGoal = new AtomicBoolean(false);
 
-
     @Override
-    public List<Integer> compute()
-    {
+    public List<Integer> compute() {
         return parallelSearch(start);
     }
 
-    private List<Integer> parallelSearch(int start){
+    private List<Integer> parallelSearch(int start) {
         // one player active on the maze at start
         int player = maze.newPlayer(start);
         // start with start node
@@ -95,7 +90,7 @@ protected class FThread{
             int current = frontier.pop();
             // if current node has a goal
             if (maze.hasGoal(current)) {
-                //global atom boolean to tell other threads goal has been found
+                // global atom boolean to tell other threads goal has been found
                 hasFoundGoal.set(true);
                 // move player to goal
                 maze.move(player, current);
@@ -110,21 +105,21 @@ protected class FThread{
                 visited.add(current);
                 // for every node nb adjacent to current
                 int nbCount = 0;
-                for (int nb: maze.neighbors(current)) {
+                for (int nb : maze.neighbors(current)) {
                     // add nb to the nodes to be processed
                     frontier.push(nb);
                     // if nb has not been already visited,
                     // nb can be reached from current (i.e., current is nb's predecessor)
                     if (!visited.contains(nb))
                         predecessor.put(nb, current);
-                        nbCount++;
-                        if (nbCount == 1) {
-                            // this is the next "current" for this thread
-                        } else  {
-                            new FThread(nb, new ForkJoinSolver(maze));
-                            
-                        } 
+                    nbCount++;
+                    if (nbCount == 1) {
+                        // this is the next "current" for this thread
+                    } else {
+                        new FThread(nb, new ForkJoinSolver(maze));
+
                     }
+                }
             }
 
         }
@@ -133,5 +128,3 @@ protected class FThread{
     }
 
 }
-
-
