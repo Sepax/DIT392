251eb10ab045f3bb1e4ce62b9fe26f512161565e
Sebastian Pålsson <seb.plson@gmail.com>
2023-09-20 23:46:29 +0200

    base

--- lab_2/amazed_lab/src/main/amazed/solver/ForkJoinSolver.java
+++ lab_2/amazed_lab/src/main/amazed/solver/ForkJoinSolver.java
@@ -2,7 +2,6 @@ package amazed.solver;
 
 import amazed.maze.Maze;
 
-import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Map;
@@ -21,25 +20,12 @@ import java.util.concurrent.ConcurrentSkipListSet;
 
 public class ForkJoinSolver
         extends SequentialSolver {
-
     /**
      * Creates a solver that searches in <code>maze</code> from the
      * start node to a goal.
      *
      * @param maze the maze to be searched
      */
-
-    // class to create new "threads" when forking
-    protected class FThread {
-        final int forkStart;
-        final ForkJoinSolver fthread;
-
-        FThread(int forkStart, ForkJoinSolver fthread) {
-            this.forkStart = forkStart;
-            this.fthread = fthread;
-        }
-    }
-
     public ForkJoinSolver(Maze maze) {
         super(maze);
     }
@@ -50,7 +36,7 @@ public class ForkJoinSolver
      * nodes.
      *
      * @param maze      the maze to be searched
-     * @param forkAFter the number of steps (visited nodes) after
+     * @param forkAfter the number of steps (visited nodes) after
      *                  which a parallel task is forked; if
      *                  <code>forkAfter &lt;= 0</code> the solver never
      *                  forks new tasks
@@ -71,60 +57,12 @@ public class ForkJoinSolver
      *         goal node in the maze; <code>null</code> if such a path cannot
      *         be found.
      */
-    AtomicBoolean hasFoundGoal = new AtomicBoolean(false);
-
     @Override
     public List<Integer> compute() {
-        return parallelSearch(start);
+        return parallelSearch();
     }
 
-    private List<Integer> parallelSearch(int start) {
-        // one player active on the maze at start
-        int player = maze.newPlayer(start);
-        // start with start node
-        frontier.push(start);
-        // as long as not all nodes have been processed
-
-        while (!frontier.empty()) {
-            // get the new node to process
-            int current = frontier.pop();
-            // if current node has a goal
-            if (maze.hasGoal(current)) {
-                // global atom boolean to tell other threads goal has been found
-                hasFoundGoal.set(true);
-                // move player to goal
-                maze.move(player, current);
-                // search finished: reconstruct and return path
-                return pathFromTo(start, current);
-            }
-            // if current node has not been visited yet
-            if (!visited.contains(current)) {
-                // move player to current node
-                maze.move(player, current);
-                // mark node as visited
-                visited.add(current);
-                // for every node nb adjacent to current
-                int nbCount = 0;
-                for (int nb : maze.neighbors(current)) {
-                    // add nb to the nodes to be processed
-                    frontier.push(nb);
-                    // if nb has not been already visited,
-                    // nb can be reached from current (i.e., current is nb's predecessor)
-                    if (!visited.contains(nb))
-                        predecessor.put(nb, current);
-                    nbCount++;
-                    if (nbCount == 1) {
-                        // this is the next "current" for this thread
-                    } else {
-                        new FThread(nb, new ForkJoinSolver(maze));
-
-                    }
-                }
-            }
-
-        }
-        // all nodes explored, no goal found
+    private List<Integer> parallelSearch() {
         return null;
     }
-
 }
