9bd887cbf1a333461fd52aa0a9a491a1ca3b127f
NacasNils <122795594+NacasNils@users.noreply.github.com>
2023-10-17 17:11:17 +0200

    fixat skit yäähni
    bara send messege grejen

--- lab-3/channel.erl
+++ lab-3/channel.erl
@@ -3,9 +3,20 @@
 
 % ---------------------------- API ---------------------------- %
 
+-record(cState, {
+    name,   % Channel name
+    users % list of clients
+}).
+
+init_state(Name) -> 
+    #cState{
+name = Name,
+users = []
+}.
+
 % Starts a new channel with the given name
 start(Name) ->
-    genserver:start(list_to_atom(Name), [], fun handle/2).
+    genserver:start(list_to_atom(Name), init_state(Name), fun handle/2).
 
 % Stops the channel with the given name
 stop(Name) ->
@@ -22,27 +33,27 @@ leave(Name, Client) ->
 % --------------------- Request Handler ---------------------- %
 
 handle(State, {join, Client}) ->
-    case lists:member(Client, State) of
+    case lists:member(Client, State#cState.users) of
         true ->
             {reply, {error, user_already_joined ,  "Already joined"}, State};
         false ->
-            {reply, ok, [Client|State]}
+            {reply, ok, State#cState{users=[Client | State#cState.users]}}
     end;
 
 handle(State, {leave, Client}) ->
-    case lists:member(Client, State) of
+    case lists:member(Client, State#cState.users) of
         true ->
-            {reply, ok, lists:delete(Client, State)};
+            {reply, ok, lists:delete(Client, State#cState.users)};
         false ->
             {reply, {error, user_not_joined , "Client is not joined"}, State}
     end;
 
 
 handle(State, {message_send, Client, Nick, Msg}) ->
-    case lists:member(Client, State) of
+    case lists:member(Client, State#cState.users) of
         true ->
-            Response = {request, self(), make_ref(), {message_receive, self(), Nick, Msg}},
-            lists:foreach(fun (Receiver) -> Receiver ! Response end, State),
+            Response = {request, self(), make_ref(), {message_receive, State#cState.name , Nick, Msg}},
+            lists:foreach(fun (Receiver) -> Receiver ! Response end, State#cState.users),
             {reply, ok, State};
         false ->
             {reply, {error, user_not_joined , "Client is not joined"}, State}
